14d875547393275ef4a661ed4dc68f8c5ed6cef2
Jason <bytianjx@outlook.com>
2016-05-20 14:12:34 +0800

    安装了环信第三方

---
+++ DuBi/classes/Helper/JTBuddyManager.h
@@ -0,0 +1,17 @@
+//
+//  JTBuddyManager.h
+//  DuBi
+//
+//  Created by Jason on 16/5/20.
+//  Copyright © 2016年 JasonTian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Singleton.h"
+@interface JTBuddyManager : NSObject
+/**
+ *  该类单例方法的声明
+ */
+singleton_interface(JTBuddyManager);
+
+@end
---
+++ DuBi/classes/Marco/Main_marco.h
@@ -0,0 +1,13 @@
+//
+//  Main_marco.h
+//  DuBi
+//
+//  Created by Jason on 16/5/20.
+//  Copyright © 2016年 JasonTian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Main_marco : NSObject
+
+@end
