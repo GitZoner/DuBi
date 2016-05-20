b19c4a0817170ef72ffa6099ac90ad15c94c2af8
Jason <bytianjx@outlook.com>
2016-05-20 08:51:10 +0800

    创建登陆和注册界面

---
+++ DuBi/classes/Scenes/SignUpAndSignIn/Controllers/ZYSignInViewController.m
@@ -0,0 +1,37 @@
+//
+//  ZYSignInViewController.m
+//  DuBi
+//
+//  Created by Jason on 16/5/20.
+//  Copyright © 2016年 JasonTian. All rights reserved.
+//
+
+#import "ZYSignInViewController.h"
+
+@interface ZYSignInViewController ()
+
+@end
+
+@implementation ZYSignInViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
