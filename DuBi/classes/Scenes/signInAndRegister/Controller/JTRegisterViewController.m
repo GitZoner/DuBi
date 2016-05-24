7b34a7deff1b795185d837768657581234bb6b21
Jason <bytianjx@outlook.com>
2016-05-24 21:22:00 +0800

    no message

--- DuBi/classes/Scenes/signInAndRegister/Controller/JTRegisterViewController.m
+++ DuBi/classes/Scenes/signInAndRegister/Controller/JTRegisterViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "JTRegisterViewController.h"
+#import <SMS_SDK/SMSSDK.h>
+#import "RegularUtils.h"
+#import "XHToast.h"
 
 @interface JTRegisterViewController ()
 // 返回按钮
@@ -44,20 +47,65 @@
 // 获取短信验证码的点击事件
 - (IBAction)requestVerCodeButtonAction:(UIButton *)sender {
     
+
+     
+    if ([RegularUtils checkTelNumber:self.telNumTextField.text]) {
+        [SMSSDK getVerificationCodeByMethod:SMSGetCodeMethodSMS phoneNumber:self.telNumTextField.text zone:@"86" customIdentifier:nil result:^(NSError *error){
+            if (!error) {
+                [XHToast showBottomWithText:@"验证码已发送" bottomOffset:200 duration:3];
+                // 打开计时器，计算验证码发送时间
+                [NSTimer timerWithTimeInterval:1 target:self selector:@selector(verCodeTimerAction:) userInfo:nil repeats:YES];
+            } else {
+                NSLog(@"错误信息：%@",error);
+            }
+         }];
+    }else {
+        // 使用第三个HUD，提示错误信息
+        [XHToast showBottomWithText:@"请输入正确的手机号" bottomOffset:200 duration:3];
+    }
+   
+  
     
+}
+-(void)verCodeTimerAction:(NSTimer *)timer {
+    static NSInteger i = 60;
+    [self.requestVerCodeButton setTitle:[NSString stringWithFormat:@"重获验证码：%ld",i ] forState:(UIControlStateNormal)];
+    NSLog(@"%ld",i);
+    --i;
+    if (i <= 0) {
+        [self.requestVerCodeButton setTitle:@"获取验证码" forState:(UIControlStateNormal)];
+        [timer invalidate];
+    }
     
 }
 
+
+
 // 注册按钮点击事件
 - (IBAction)registerButtonAction:(id)sender {
-    
-    
+    // 提交验证码
+    /*
+    [SMSSDK commitVerificationCode:self.verCodeTextField.text phoneNumber:_telNumTextField.text zone:@"86" result:^(NSError *error) {
+        
+        if (!error) {
+
+        }
+        else
+        {
+            NSLog(@"错误信息:%@",error);
+        }
+    }];
+     */
 }
 
 // 返回按钮事件
 - (IBAction)goBackButtonAction:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
+    
 }
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
